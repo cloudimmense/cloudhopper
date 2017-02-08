f8dc63ed02c4adaf09d0b17bb6a23a2a4d110ad7
samvaran <samvaran2best@gmail.com>
2017-02-08 02:40:46 +0000

    updated views boilerplate

--- cloudhopper/urls.py
+++ cloudhopper/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework.urlpatterns import format_suffix_patterns
+
 
 urlpatterns = [
     url(r'^', include('restycloud.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
