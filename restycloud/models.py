16c2145199549255ecbb3d20d8d1a1eac6a73950
samvaran <samvaran2best@gmail.com>
2017-02-11 22:36:57 +0000

    added models for gcloud , openstack , rackspace and dropbox

--- restycloud/models.py
+++ restycloud/models.py
@@ -32,3 +32,63 @@ class AWSCredentials(models.Model):
 
     class Meta:
         ordering = ('created',)
+
+
+class GcloudCredentials(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    cred_name = models.CharField(max_length=100, blank=True)
+    type = models.CharField(max_length=100, blank=False)
+    project_id = models.CharField(max_length=100, blank=False)
+    private_key_id = models.CharField(max_length=100, blank=False)
+    private_key = models.TextField()
+    client_email = models.CharField(max_length=100, blank=False)
+    client_id = models.CharField(max_length=100, blank=False)
+    auth_uri = models.CharField(max_length=100, blank=False)
+    token_uri = models.CharField(max_length=100, blank=False)
+    auth_provider_x509_cert_url = models.CharField(max_length=100, blank=False)
+    client_x509_cert_url = models.CharField(max_length=100, blank=False)
+    json_meta = models.TextField()
+    json_file_path = models.CharField(max_length=100, blank=True)
+    meta = models.TextField()
+
+    class Meta:
+        ordering = ('created',)
+
+
+class OpenstackCredentials(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    cred_name = models.CharField(max_length=100, blank=True)
+    auth_url = models.CharField(max_length=100, blank=False)
+    username = models.CharField(max_length=100, blank=False)
+    password = models.CharField(max_length=100, blank=False)
+    project = models.CharField(max_length=100, blank=False)
+    clouds_yaml = models.TextField()
+    assoc_acc = models.CharField(max_length=100, blank=False)
+    meta = models.TextField()
+
+    class Meta:
+        ordering = ('created',)
+
+
+class RackspaceCredentials(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    cred_name = models.CharField(max_length=100, blank=False)
+    username = models.CharField(max_length=100, blank=False)
+    api_key = models.CharField(max_length=100, blank=False)
+    secret_key = models.CharField(max_length=100, blank=False)
+    assoc_acc = models.CharField(max_length=100, blank=False)
+    meta = models.TextField()
+    
+    class Meta:
+        ordering = ('created',)
+
+
+class DropBoxCredentials(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    cred_name = models.CharField(max_length=100, blank=True)
+    access_token = models.CharField(max_length=100, blank=False)
+    assoc_acc = models.CharField(max_length=100, blank=False)
+    meta = models.TextField()
+
+    class Meta:
+        ordering = ('created',)
