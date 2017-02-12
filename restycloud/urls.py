ff1c82c18b60855a5bd26b3c4f6dc73e0c369dbc
samvaran <samvaran2best@gmail.com>
2017-02-12 01:28:00 +0000

    bugfix: resolved the no fields error by adding serialzer attributes to classes

--- restycloud/urls.py
+++ restycloud/urls.py
@@ -7,6 +7,14 @@ urlpatterns = [
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
     url(r'^credentials/aws/$', views.AWSCredentialList.as_view()),
     url(r'^credentials/aws/(?P<pk>[0-9]+)/$', views.AWSCredentialDetail.as_view()),
+    url(r'^credentials/openstack/$', views.OpenstackCredentialList.as_view()),
+    url(r'^credentials/openstack/(?P<pk>[0-9]+)/$', views.OpenstackCredentialDetail.as_view()),
+    url(r'^credentials/gcloud/$', views.GcloudCredentialList.as_view()),
+    url(r'^credentials/gcloud/(?P<pk>[0-9]+)/$', views.GcloudCredentialDetail.as_view()),
+    url(r'^credentials/rackspace/$', views.RackspaceCredentialList.as_view()),
+    url(r'^credentials/rackspace/(?P<pk>[0-9]+)/$', views.RackspaceCredentialDetail.as_view()),
+    url(r'^credentials/dropbox/$', views.DropBoxCredentialList.as_view()),
+    url(r'^credentials/dropbox/(?P<pk>[0-9]+)/$', views.DropBoxCredentialDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- restycloud/views.py
+++ restycloud/views.py
@@ -5,9 +5,13 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from restycloud.models import Snippet
-from restycloud.models import AWSCredentials
+from restycloud.models import * 
 from restycloud.serializers import SnippetSerializer
 from restycloud.serializers import AWSCredentialsSerializer
+from restycloud.serializers import GcloudCredentialsSerializer
+from restycloud.serializers import DropBoxCredentialsSerializer
+from restycloud.serializers import OpenstackCredentialsSerializer
+from restycloud.serializers import RackspaceCredentialsSerializer
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -15,6 +19,7 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import generics, status
 
 
 class SnippetList(APIView):
@@ -23,6 +28,8 @@ class SnippetList(APIView):
     """
     permission_classes = (IsAuthenticated, )
     authentication_classes = (TokenAuthentication, )
+    serializer_class = SnippetSerializer
+
 
     def get(self, request, format=None):
         snippets = Snippet.objects.all()
@@ -41,6 +48,8 @@ class SnippetDetail(APIView):
     """
     Retrieve, update or delete a snippet instance.
     """
+    serializer_class = SnippetSerializer
+    
     def get_object(self, pk):
         try:
             return Snippet.objects.get(pk=pk)
@@ -50,7 +59,8 @@ class SnippetDetail(APIView):
     def get(self, request, pk, format=None):
         snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
+    serializer_class = SnippetSerializer
+
 
     def put(self, request, pk, format=None):
         snippet = self.get_object(pk)
@@ -70,7 +80,8 @@ class AWSCredentialList(APIView):
     """
     List all credentials, or create a new credentials.
     """
-    
+    serializer_class = AWSCredentialsSerializer
+
     def get(self, request, format=None):
         creds = AWSCredentials.objects.all()
         serializer = AWSCredentialsSerializer(creds, many=True)
@@ -88,6 +99,8 @@ class AWSCredentialDetail(APIView):
     """
     Retrieve, update or delete a AWSCredential instance.
     """
+    serializer_class = AWSCredentialsSerializer
+
     def get_object(self, pk):
         try:
             return AWSCredentials.objects.get(pk=pk)
@@ -113,3 +126,201 @@ class AWSCredentialDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+class GcloudCredentialList(APIView):
+    """
+    List all credentials, or create a new Gcloud credentials.
+    """
+    serializer_class = GcloudCredentialsSerializer
+
+    def get(self, request, format=None):
+        creds = GcloudCredentials.objects.all()
+        serializer = GcloudCredentialsSerializer(creds, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = GcloudCredentialsSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GcloudCredentialDetail(APIView):
+    """
+    Retrieve, update or delete a Gcloud Credential instance.
+    """
+    serializer_class = GcloudCredentialsSerializer
+
+    def get_object(self, pk):
+        try:
+            return GcloudCredentials.objects.get(pk=pk)
+        except GcloudCredentials.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        cred = self.get_object(pk)
+        serializer = GcloudCredentialsSerializer(cred)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        cred = self.get_object(pk)
+        serializer = GcloudCredentialsSerializer(cred, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        cred = self.get_object(pk)
+        cred.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+
+class OpenstackCredentialList(APIView):
+    """
+    List all credentials, or create a new Gcloud credentials.
+    """
+    serializer_class = OpenstackCredentialsSerializer
+    
+    def get(self, request, format=None):
+        creds = OpenstackCredentials.objects.all()
+        serializer = OpenstackCredentialsSerializer(creds, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = OpenstackCredentialsSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class OpenstackCredentialDetail(APIView):
+    """
+    Retrieve, update or delete a Gcloud Credential instance.
+    """
+    serializer_class = OpenstackCredentialsSerializer
+    
+    def get_object(self, pk):
+        try:
+            return OpenstackCredentials.objects.get(pk=pk)
+        except OpenstackCredentials.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        cred = self.get_object(pk)
+        serializer = OpenstackCredentialsSerializer(cred)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        cred = self.get_object(pk)
+        serializer = OpenstackCredentialsSerializer(cred, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        cred = self.get_object(pk)
+        cred.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class RackspaceCredentialList(APIView):
+    """
+    List all credentials, or create a new Rackspace credentials.
+    """
+    serializer_class = RackspaceCredentialsSerializer
+    
+    def get(self, request, format=None):
+        creds = RackspaceCredentials.objects.all()
+        serializer = RackspaceCredentialsSerializer(creds, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = RackspaceCredentialsSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RackspaceCredentialDetail(APIView):
+    """
+    Retrieve, update or delete a Gcloud Credential instance.
+    """
+    serializer_class = RackspaceCredentialsSerializer
+    
+    def get_object(self, pk):
+        try:
+            return RackspaceCredentials.objects.get(pk=pk)
+        except RackspaceCredentials.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        cred = self.get_object(pk)
+        serializer = RackspaceCredentialsSerializer(cred)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        cred = self.get_object(pk)
+        serializer = RackspaceCredentialsSerializer(cred, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        cred = self.get_object(pk)
+        cred.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class DropBoxCredentialList(APIView):
+    """
+    List all credentials, or create a new DropBox credentials.
+    """
+    serializer_class = DropBoxCredentialsSerializer
+    
+    def get(self, request, format=None):
+        creds = DropBoxCredentials.objects.all()
+        serializer = DropBoxCredentialsSerializer(creds, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = DropBoxCredentialsSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DropBoxCredentialDetail(APIView):
+    """
+    Retrieve, update or delete a DropBox Credential instance.
+    """
+    serializer_class = DropBoxCredentialsSerializer
+    
+    def get_object(self, pk):
+        try:
+            return DropBoxCredentials.objects.get(psk=pk)
+        except DropBoxCredentials.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        cred = self.get_object(pk)
+        serializer = DropBoxCredentialsSerializer(cred)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        cred = self.get_object(pk)
+        serializer = DropBoxCredentialsSerializer(cred, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        cred = self.get_object(pk)
+        cred.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
